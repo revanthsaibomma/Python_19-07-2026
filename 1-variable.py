85fc0231cdb929a7d7bdd6dc289d3de85153ade3
revanthsaibomma <revanthsaibomma807@gmail.com>
2026-07-19 16:04:54 +0530

    Remove unwanted files

--- 1-variable.py
+++
@@ -1,7 +0,0 @@
-#Define the variable
-str = "Manav Rachna!"
-print("The variable str would be printed as: ", str)
-print(f"The variable str would be printed as: {str}")
-i = 10
-print(i)
-print(f"The variable i would be printed as: {i}")
--- 6-vowelconsonant.py
+++
@@ -1,5 +0,0 @@
-char = input("Enter a character: ").strip().lower()
-if char in ['a', 'e', 'i', 'o', 'u']:
-    print(f"The character {char} is a vowel.")
-else:
-    print(f"The character {char} is a consonant.")
